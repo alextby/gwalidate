5e4e98075742c901551a8758ab8e3a30a4829f1c
alextby <iamrdale@gmail.com>
2014-08-19 22:45:03 +0300

    Fixed a few markup issues

--- gwalidate-playground/src/main/java/com/github/alextby/ui/gwt/gwalidate/demo/client/mvp/view/usecase/BaseUseCaseView.java
+++ gwalidate-playground/src/main/java/com/github/alextby/ui/gwt/gwalidate/demo/client/mvp/view/usecase/BaseUseCaseView.java
@@ -2,10 +2,10 @@ package com.github.alextby.ui.gwt.gwalidate.demo.client.mvp.view.usecase;
 
 import com.github.alextby.ui.gwt.gwalidate.core.engine.ValidationPanel;
 import com.github.alextby.ui.gwt.gwalidate.core.engine.ValidatorDelegate;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.SubmitButton;
 
@@ -19,17 +19,28 @@ public abstract class BaseUseCaseView extends Composite {
 
     ValidatorDelegate delegate;
 
-    @UiField
+    @UiField(provided = true)
     SubmitButton validateBtn;
+
+    private Element validateBtnElement;
 
     public BaseUseCaseView(ValidationPanel panel) {
         this.validationPanel = panel;
+        validateBtn = new SubmitButton();
+        validateBtn.setText("Validate");
+        validateBtnElement = validateBtn.getElement();
+        validateBtnElement.addClassName("btn btn-large btn-primary");
     }
 
     @UiHandler("validateBtn")
     public void onValidateBtnCick(ClickEvent event) {
+        validateBtnElement.removeClassName("btn-primary");
         if (validationPanel.validate().isValid()) {
-            Window.alert("Validation went well!");
+            validateBtnElement.removeClassName("btn-danger");
+            validateBtnElement.addClassName("btn-success");
+        } else {
+            validateBtnElement.removeClassName("btn-success");
+            validateBtnElement.addClassName("btn-danger");
         }
     }
 
